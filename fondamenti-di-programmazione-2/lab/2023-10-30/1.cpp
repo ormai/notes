a410aaec05624e2d914d63f4c0fa995435734cb2
ormai <mariodandrea@mailfence.com>
2023-10-27 16:29:53 +0200

    Esercizi 30 Ottobre

---
+++ fondamenti-di-programmazione-2/lab/2023-10-30/1.cpp
@@ -0,0 +1,138 @@
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout, std::endl;
+
+class File {
+protected:
+  string nome;
+  unsigned dimensione; // dimensione in byte
+
+public:
+  File() {}
+  File(const string &_nome, const unsigned &_dimensione)
+      : nome(_nome), dimensione(_dimensione) {}
+  File(const File &file) {
+    nome = file.getNome();
+    dimensione = file.getDimensione();
+  }
+
+  File &operator=(const File &file) {
+    nome = file.getNome();
+    dimensione = file.getDimensione();
+    return *this;
+  }
+
+  void rinomina(const string &nuovoNome) { nome = nuovoNome; }
+
+  string getNome() const { return nome; }
+  unsigned getDimensione() const { return dimensione; }
+};
+
+class FilePdf : public File {
+private:
+  bool firmato;
+
+public:
+  FilePdf() {}
+  FilePdf(const string &_nome, const unsigned &_dimensione, const bool _firmato)
+      : File(_nome, _dimensione), firmato(_firmato) {}
+  FilePdf(const FilePdf &pdf)
+      : File(pdf.getNome(), pdf.getDimensione()), firmato(pdf.eFirmato()) {}
+
+  FilePdf &operator=(const FilePdf &pdf) {
+    nome = pdf.getNome();
+    dimensione = pdf.getDimensione();
+    firmato = pdf.eFirmato();
+    return *this;
+  }
+
+  bool eFirmato() const { return firmato; }
+};
+
+class FileAudio : public File {
+private:
+  unsigned durata; // durata in secondi
+
+public:
+  FileAudio() {}
+  FileAudio(const string &_nome, const unsigned &_dimensione,
+            const unsigned &_durata)
+      : File(_nome, _dimensione), durata(_durata) {}
+  FileAudio(const FileAudio &audio)
+      : File(audio.getNome(), audio.getDimensione()),
+        durata(audio.getDurata()) {}
+
+  FileAudio &operator=(const FileAudio &audio) {
+    nome = audio.getNome();
+    dimensione = audio.getDurata();
+    durata = audio.getDurata();
+    return *this;
+  }
+
+  unsigned getDurata() const { return durata; }
+};
+
+class FileArchivio : public File {
+  vector<File *> archivio;
+  double fattoreDiCompressione;
+
+public:
+  FileArchivio() {}
+  FileArchivio(const double &_fattoreDiCompressionee)
+      : fattoreDiCompressione(_fattoreDiCompressionee) {}
+  FileArchivio(const FileArchivio &fa)
+      : File(fa.getNome(), fa.getDimensione()),
+        fattoreDiCompressione(fa.getFattoreDiCompressione()) {
+    for (unsigned i = 0; i < fa.getNumeroFile(); ++i)
+      archivio.push_back(fa[i]);
+  }
+
+  double getFattoreDiCompressione() const { return fattoreDiCompressione; }
+  void setFattoreDiCompressione(const double &_fattoreDiCompressione) {
+    fattoreDiCompressione = _fattoreDiCompressione;
+  }
+  unsigned getDimensione() const;
+  void archivia(File *file) { archivio.push_back(file); }
+  unsigned getNumeroFile() const { return archivio.size(); }
+
+  File *operator[](const unsigned i) const { return archivio[i]; }
+  File *&operator[](const unsigned i) { return archivio[i]; }
+
+  FileArchivio &operator=(const FileArchivio &fa) {
+    nome = fa.getNome();
+    dimensione = fa.getDimensione();
+
+    for (unsigned i = 0; i < fa.getNumeroFile(); ++i)
+      archivio.push_back(fa[i]);
+
+    fattoreDiCompressione = fa.getFattoreDiCompressione();
+
+    return *this;
+  }
+};
+
+unsigned FileArchivio::getDimensione() const {
+  unsigned dimensioneArchivio = 0;
+  for (unsigned i = 0; i < archivio.size(); ++i)
+    dimensioneArchivio += archivio[i]->getDimensione();
+  return dimensioneArchivio * fattoreDiCompressione;
+}
+
+int main() {
+  // TESTING
+  FilePdf manualeCPP("C++ Primer.pdf", 46336275, false);
+  FileAudio numb("LinkinPark-Numb.mp3", 368742, 185);
+  File appunti("Polimorfismo.txt", 1762);
+
+  FileArchivio zip(0.7);
+  zip.archivia(&manualeCPP);
+  zip.archivia(&numb);
+  zip.archivia(&appunti);
+
+  cout << zip.getDimensione() << endl;
+
+  return 0;
+}
---
+++ fondamenti-di-programmazione-2/lab/2023-10-30/2.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using std::cout, std::endl;
+#include <vector>
+using std::vector;
+#include <algorithm>
+#include <cmath> // M_PI
+using std::sort;
+
+class FiguraGeometrica {
+public:
+  virtual double perimetro() const = 0; // perimetro dell
+  virtual double area() const = 0;      // area della figura
+  virtual void stampa() const = 0;
+};
+
+class Cerchio : public FiguraGeometrica {
+private:
+  double raggio;
+
+public:
+  Cerchio() {}
+  Cerchio(const double &_raggio) : raggio(_raggio) {}
+  Cerchio(const Cerchio &cerchio) : raggio(cerchio.raggio) {}
+
+  double perimetro() const { return 2 * M_PI * raggio; }
+  double area() const { return M_PI * raggio * raggio; }
+
+  void stampa() const {
+    cout << "Cerchio, raggio: " << raggio << " mm." << endl;
+  }
+};
+
+class Quadrato : public FiguraGeometrica {
+private:
+  double lato;
+
+public:
+  Quadrato() {}
+  Quadrato(const double &_lato) : lato(_lato) {}
+  Quadrato(const Quadrato &quadrato) : lato(quadrato.lato) {}
+
+  double perimetro() const { return 4 * lato; }
+  double area() const { return lato * lato; }
+
+  double getLato() const { return lato; }
+
+  void stampa() const {
+    cout << "Quadrato, lato: " << getLato() << " mm." << endl;
+  }
+};
+
+class Rettangolo : public FiguraGeometrica {
+private:
+  double latoMaggiore;
+  double latoMinore;
+
+public:
+  Rettangolo() {}
+  Rettangolo(const double &_latoMaggiore, const double &_latoMinore)
+      : latoMaggiore(_latoMaggiore), latoMinore(_latoMinore) {}
+  Rettangolo(const Rettangolo &rettangolo)
+      : latoMaggiore(rettangolo.getLatoMaggiore()),
+        latoMinore(rettangolo.getLatoMinore()) {}
+
+  double getLatoMaggiore() const { return latoMaggiore; }
+  double getLatoMinore() const { return latoMinore; }
+  double perimetro() const { return 2 * latoMaggiore + 2 * latoMinore; }
+  double area() const { return latoMaggiore * latoMinore; }
+
+  void stampa() const {
+    cout << "Rettangolo " << getLatoMaggiore() << 'x' << getLatoMinore()
+         << " mm." << endl;
+  }
+};
+
+int main() {
+  Cerchio c(8.756);
+  Cerchio d(14.31);
+
+  Rettangolo r(40, 15);
+  Rettangolo s(20, 10);
+
+  Quadrato q(20);
+
+  vector<FiguraGeometrica *> figure;
+  figure.push_back(&c);
+  figure.push_back(&d);
+  figure.push_back(&r);
+  figure.push_back(&s);
+  figure.push_back(&q);
+
+  sort(figure.begin(), figure.end(),
+       [](FiguraGeometrica *a, FiguraGeometrica *b) {
+         return (a->area() / a->perimetro()) > (b->area() / b->perimetro());
+       });
+
+  for (FiguraGeometrica *figura : figure)
+    figura->stampa();
+
+  return 0;
+}
